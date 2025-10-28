9495ea2d2add9b945750d322e16d6c136c4a0021
JamesGames7 <crask26@pickeringcollege.on.ca>
2025-10-28 14:39:13 -0400

    Finalizing TS setup

---
+++ src/deepregrets.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+GameGui = (function () { // this hack required so we fake extend GameGui
+  function GameGui() {}
+  return GameGui;
+})();
+
+// Note: it does not really extend it in es6 way, you cannot call super you have to use dojo way 
+class YourGameName extends GameGui<YourGameNameGamedatas> { 
+     constructor() {
+        super();
+     }
+     
+     public setup(gamedatas: any) {
+         this.setupNotifications();
+     } 
+     public onEnteringState(stateName: string, args: any) {}
+     public onLeavingState(stateName: string) {}
+     public onUpdateActionButtons(stateName: string, args: any) {} 
+     public setupNotifications() {}
+}
