77e3edfaa4851b1556ad807c3204c5a62021089b
JamesGames7 <crask26@pickeringcollege.on.ca>
2025-10-28 19:34:28 -0400

    Skeleton for TS files

---
+++ deepregrets.js
@@ -0,0 +1,36 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// @ts-ignore
+GameGui = (function () {
+    function GameGui() { }
+    return GameGui;
+})();
+// Note: it does not really extend it in es6 way, you cannot call super you have to use dojo way 
+var DeepRegrets = /** @class */ (function (_super) {
+    __extends(DeepRegrets, _super);
+    function DeepRegrets() {
+        //@ts-ignore
+        return _super.call(this) || this;
+    }
+    DeepRegrets.prototype.setup = function (gamedatas) {
+        this.setupNotifications();
+    };
+    DeepRegrets.prototype.onEnteringState = function (stateName, args) { };
+    DeepRegrets.prototype.onLeavingState = function (stateName) { };
+    DeepRegrets.prototype.onUpdateActionButtons = function (stateName, args) { };
+    DeepRegrets.prototype.setupNotifications = function () { };
+    return DeepRegrets;
+}(GameGui));
